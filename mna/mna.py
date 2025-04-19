2c8860db816adee3e9137cec1f7e10d1ec260bbb
Ramil Khasanshin <khasanshin.ramil@list.ru>
2025-04-19 21:15:39 +0300

    WIP: Added class & parse method

--- mna/mna.py
+++ mna/mna.py
@@ -1,3 +1,20 @@
 #!/usr/bin/python3
 
+import sys
+import sympy as sp
 
+# C L A S S
+
+class Circuit:
+    def __init__():
+        self.data = []
+        self.nodeCount = 0
+        self.nodeNames = []
+
+
+# P A R S E
+"""
+
+"""
+def ParseCircuit(text):
+    pass
