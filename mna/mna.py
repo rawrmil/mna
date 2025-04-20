6ba27628e1bcd4afc2ba655169b48b25d5c6e513
Ramil Khasanshin <khasanshin.ramil@list.ru>
2025-04-21 00:47:21 +0300

    Parsing done

--- mna/mna.py
+++ mna/mna.py
@@ -3,12 +3,13 @@
 import re
 import sys
 import sympy as sp
+from sympy.parsing.sympy_parser import parse_expr, standard_transformations
 from typing import List
 
 """
 INPUT:
 
-./mna.py node1 node2 type_name expr; ...
+./mna.py [-complex] node1 node2 type_name expr; ...
 
 CONSTRAINTS:
 
@@ -17,45 +18,60 @@ type: i/I, u/U, r/R
 name: a-zA-Z0-9
 expr: ???
 """
-    
+
+# S T A T E
+
+is_complex=False
+
+# E R R O R
+
+def Error(name):
+    sys.stderr.write(name)
+    exit(1)
+
 # P A R S E
 
 def ProcessElementProperties(prop):
     name_pattern = re.compile("^[a-zA-Z0-9_-]*$")
     if len(prop) < 4:
-        sys.stderr.write("argument-shortage")
-    if len(prop) > 4:
-        sys.stderr.write("argument-overage")
+        Error("parse-argument-shortage")
     if not name_pattern.match(prop[0]):
-        sys.stderr.write("pattern-node-src")
-    if not node_pattern.match(prop[1]):
-        sys.stderr.write("pattern-node-dst")
+        Error("parse-pattern-node-src")
+    if not name_pattern.match(prop[1]):
+        Error("parse-pattern-node-dst")
     prop[2] = prop[2].split("_", 1)
-    if len(prop[2]) == 1:
-        sys.stderr.write("name-type-separator")
+    if len(prop[2]) != 2:
+        Error("parse-name-type-separator")
     if prop[2][0] == '':
-        sys.stderr.write("empty-type")
+        Error("parse-empty-type")
     if prop[2][1] == '':
-        sys.stderr.write("empty-name")
+        Error("parse-empty-name")
     if not name_pattern.match(prop[2][1]):
-        sys.stderr.write("pattern-name")
-    if not prpp[2][0].lower() in ['i', 'u', 'r']:
-        sys.stderr.write("no-such-etype")
+        Error("parse-pattern-name")
+    if not prop[2][0].lower() in ['i', 'u', 'r']:
+        Error("parse-no-such-etype")
+    prop[3] = parse_expr(prop[3])
+    if is_complex:
+        syms = prop[3].free_symbols
+        complex_syms = {s: symbols(s.name, complex=True) for s in syms}
+        prop[3] = expr.subs(complex_syms)
 
 def ParseCircuit(text: str):
-    prop_list = [e.strip().split() for e in text.split(';')]
+    prop_list = [e.strip().split(None, 3) for e in text.split(';')]
     for i, p in enumerate(prop_list):
         ProcessElementProperties(p)
+    print(prop_list)
 
 # S O L V E
 
-def Solve(self):
+def Solve(circuit):
     pass
-
 
 if __name__ == "__main__":
     if len(sys.argv) == 1:
         exit(1)
-    text = " ".join(sys.argv[1:])
-    circuit = Circuit(text)
-    circuit.Solve()
+    if "-complex" in sys.argv:
+        is_complex=True
+        sys.argv.delete("-complex")
+    circuit = ParseCircuit(" ".join(sys.argv[1:]))
+    Solve(circuit)
