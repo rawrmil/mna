887a7eb13e420f55d8d112d7c4ddaef3746125b8
Ramil Khasanshin <khasanshin.ramil@list.ru>
2025-04-21 01:35:30 +0300

    WIP: Adding solving

--- mna/mna.py
+++ mna/mna.py
@@ -9,7 +9,7 @@ from typing import List
 """
 INPUT:
 
-./mna.py [-complex] node1 node2 type_name expr; ...
+./mna.py node1 node2 type_name expr; ...
 
 CONSTRAINTS:
 
@@ -19,59 +19,74 @@ name: a-zA-Z0-9
 expr: ???
 """
 
-# S T A T E
-
-is_complex=False
-
 # E R R O R
 
 def Error(name):
     sys.stderr.write(name)
+    print()
     exit(1)
 
 # P A R S E
 
-def ProcessElementProperties(prop):
+def ProcessElementProperties(element):
     name_pattern = re.compile("^[a-zA-Z0-9_-]*$")
-    if len(prop) < 4:
+    if len(element) < 4:
         Error("parse-argument-shortage")
-    if not name_pattern.match(prop[0]):
+    if not name_pattern.match(element[0]):
         Error("parse-pattern-node-src")
-    if not name_pattern.match(prop[1]):
+    if not name_pattern.match(element[1]):
         Error("parse-pattern-node-dst")
-    prop[2] = prop[2].split("_", 1)
-    if len(prop[2]) != 2:
+    element[2] = element[2].split("_", 1)
+    if len(element[2]) != 2:
         Error("parse-name-type-separator")
-    if prop[2][0] == '':
+    if element[2][0] == '':
         Error("parse-empty-type")
-    if prop[2][1] == '':
+    if element[2][1] == '':
         Error("parse-empty-name")
-    if not name_pattern.match(prop[2][1]):
+    if not name_pattern.match(element[2][1]):
         Error("parse-pattern-name")
-    if not prop[2][0].lower() in ['i', 'u', 'r']:
+    if not element[2][0].lower() in ['i', 'u', 'r']:
         Error("parse-no-such-etype")
-    prop[3] = parse_expr(prop[3])
-    if is_complex:
-        syms = prop[3].free_symbols
-        complex_syms = {s: symbols(s.name, complex=True) for s in syms}
-        prop[3] = expr.subs(complex_syms)
+    element[3] = parse_expr(element[3])
 
 def ParseCircuit(text: str):
+    # TODO: Unique name check
     prop_list = [e.strip().split(None, 3) for e in text.split(';')]
-    for i, p in enumerate(prop_list):
-        ProcessElementProperties(p)
-    print(prop_list)
+    for i, e in enumerate(prop_list):
+        ProcessElementProperties(e)
+    return prop_list
 
 # S O L V E
 
 def Solve(circuit):
-    pass
+    unique_nodes = set()
+    for e in circuit:
+        unique_nodes.add(e[0])
+        unique_nodes.add(e[1])
+    unique_nodes = sorted(unique_nodes)
+    potentials = [sp.Symbol(f"p_{n}") for n in unique_nodes]
+    unknown_currents = []
+    eq_list = [0]*len(unique_nodes)
+    for e in circuit:
+        print(eq_list)
+        etype = e[2][0].lower()
+        node1 = unique_nodes.index(e[0])
+        node2 = unique_nodes.index(e[1])
+        if etype == "r":
+            eq_list[node1] -= potentials[node1]/e[3]
+            eq_list[node2] += potentials[node2]/e[3]
+        if etype == "i":
+            eq_list[node1] -= e[3]
+            eq_list[node2] += e[3]
+        if etype == "u":
+            unknown_currents.append(sp.Symbol(f"i_{e[2][1]}"))
+            eq_list[node1] -= unknown_currents[-1]
+            eq_list[node2] += unknown_currents[-1]
+            eq_list.append(potentials[node1]-potentials[node2])
+    print(eq_list)
 
 if __name__ == "__main__":
     if len(sys.argv) == 1:
         exit(1)
-    if "-complex" in sys.argv:
-        is_complex=True
-        sys.argv.delete("-complex")
     circuit = ParseCircuit(" ".join(sys.argv[1:]))
     Solve(circuit)
