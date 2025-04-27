e219278df314933c773298bf66294958e947fdc0
Ramil Khasanshin <khasanshin.ramil@list.ru>
2025-04-27 16:25:00 +0300

    Added variables parsing

--- mna/mna.py
+++ mna/mna.py
@@ -4,12 +4,41 @@
 
 import sys
 import yaml
+import sympy as sp
+from sympy.parsing.mathematica import parse_mathematica
+
+def ParseVariable(var, local_vars):
+    # TODO: Check if name is right
+    # TODO: Check if name is unique
+    name = var["name"]
+    value = sp.Symbol(name)
+    local_vars[name] = value
+
+#def ParseElement(elem, local_vars):
+#    parameters = el["parameters"]
+#    for param in parameters.items():
+#        key, value = param
+#        parameters[key] = parse_mathematica(value, locals=local_vars)
+
 
 def Parse(fname):
     # TODO: Structural checks
     circuit_file = open(sys.argv[1], "r")
-    circuit = yaml.safe_load(circuit_file)
+    circuit_data = yaml.safe_load(circuit_file)
     circuit_file.close()
+    circuit = circuit_data["circuit"]
+    # Parse variables
+    variables = circuit["variables"]
+    local_vars = {}
+    for var in variables:
+        ParseVariable(var, local_vars)
+    print(variables)
+    print(local_vars)
+    # Parse elements
+    #elements = circuit["elements"]
+    #for elem in elements:
+    #    ParseElement(elem, local_vars)
+    #return circuit_data
 
 def Solve(circuit):
     pass
@@ -19,4 +48,4 @@ if __name__ == "__main__":
         # TODO: Erro handling
         print("pipenv ./mna.py [file]")
         exit(1)
-    Perse(sys.argv[1])
+    circuit_data = Parse(sys.argv[1])
