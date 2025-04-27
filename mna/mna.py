92d5e860ea8b3b240e60ee7caeb0506819c94cc8
Ramil Khasanshin <khasanshin.ramil@list.ru>
2025-04-27 15:01:24 +0300

    Reading YAML example

--- mna/mna.py
+++ mna/mna.py
@@ -1,11 +1,22 @@
 #!/usr/bin/python3
 
+# TODO: Strict typing
+
+import sys
+import yaml
+
 def Parse(fname):
-    pass
+    # TODO: Structural checks
+    circuit_file = open(sys.argv[1], "r")
+    circuit = yaml.safe_load(circuit_file)
+    circuit_file.close()
 
 def Solve(circuit):
     pass
 
 if __name__ == "__main__":
-    pass
-
+    if len(sys.argv) != 2:
+        # TODO: Erro handling
+        print("pipenv ./mna.py [file]")
+        exit(1)
+    Perse(sys.argv[1])
